381546d7e9974aa29d6dbb4491554c95c94fa167
achraffawzi <achraffawzi224a@gmail.com>
2021-04-23 17:11:03 +0200

    Initial Push

---
+++ JobPostingProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JobPostingProject.Startup))]
+namespace JobPostingProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
