b60c4bd911ea0f99967405534ef0e7bf2481ab8c
achraffawzi <achraffawzi224a@gmail.com>
2021-04-26 18:17:48 +0200

    Adding roles

---
+++ JobPostingProject/Controllers/ApplicationBaseController.cs
@@ -0,0 +1,26 @@
+﻿using JobPostingProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JobPostingProject.Controllers
+{
+    public class ApplicationBaseController : Controller
+    {
+        protected override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            if(User != null)
+            {
+                ApplicationDbContext context = new ApplicationDbContext();
+                var userName = User.Identity.Name;
+                var user = context.Users.SingleOrDefault(u => u.UserName.Equals(userName));
+                string fullName = String.Concat(new string[] { user.FirstName, " ", user.LastName });
+
+                ViewData["FullName"] = fullName;
+            }
+            base.OnActionExecuted(filterContext);
+        }
+    }
+}
